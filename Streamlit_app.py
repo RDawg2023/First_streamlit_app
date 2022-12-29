cb9e2c7ae926bcecb53244ece7646bb956ff0f94
RDawg2023 <121637447+RDawg2023@users.noreply.github.com>
2022-12-29 14:30:13 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,7 +3,12 @@ import streamlit
 streamlit.title('My Paretnes new Healty Diner')
 
 streamlit.header('Breakfast Menu')
+
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('avacodo toast')
+
+Streamlit.header('Build Your Own Smoothie')
+
 
