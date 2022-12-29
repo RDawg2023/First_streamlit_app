f05222716dd2fd9713f2351c99013617800c89b5
RDawg2023 <121637447+RDawg2023@users.noreply.github.com>
2022-12-29 14:29:06 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Paretnes new Healty Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
