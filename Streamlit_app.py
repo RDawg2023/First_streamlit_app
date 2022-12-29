3f85716e5e8d113935f74bb3d9d938747f380ef8
RDawg2023 <121637447+RDawg2023@users.noreply.github.com>
2022-12-29 14:31:25 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -9,6 +9,6 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('avacodo toast')
 
-streamlit.header('Build Your Own Smoothie')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
